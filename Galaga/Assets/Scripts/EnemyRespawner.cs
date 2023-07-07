6e93d94d3d2cb016b286bb544f046dbaba5680e4
DESKTOP-A3KKB44\KGA <sjbh3mc@gmail.com>
2023-07-07 13:30:00 +0900

    update (7.7) logic updated

--- Galaga/Assets/Scripts/EnemyRespawner.cs
+++ Galaga/Assets/Scripts/EnemyRespawner.cs
@@ -6,21 +6,34 @@ using static UnityEngine.GraphicsBuffer;
 
 public class EnemyRespawner : MonoBehaviour
 {
+    // Enemy 그룹을 생성해서 내려보낼 출발점 1, 2
+    public GameObject enemyRespawnPoint1;
+    public GameObject enemyRespawnPoint2;
+
+    // 불러올 Enemy 개체
     public GameObject enemyPrefab;
-    public float speed = 5.0f;
+    //public float speed = 5.0f;
 
-    public float spawnRateMin = 1.5f;
-    public float spawnRateMax = 4.0f;
+    // 스폰할 시간 간격
+    public float spawnRateMin = 6.0f;
+    public float spawnRateMax = 8.0f;
 
+    // 스폰 시간 변수
     private float spawnRate;
     private float timeAfterSpawn;
 
-    //private GameObject[] enemyArrays;
-    //private int arraysize = 3;
+    // 첫 Enemy 스폰 포지션
+    private Vector3 startSpawnLocation1;
+    private Vector3 startSpawnLocation2;
 
-    private Vector3 spawnposition1;
-    private Vector3 spawnposition2;
-    private Vector3 spawnposition3;
+    // Enemy의 행렬 설정
+    private float row = 2.0f;
+    private float column = 3.0f;
+    // Enemy 몇칸 띄울지 변수 선언
+    private float spaceX = 2.0f;
+    private float spaceZ = 2.0f;
+
+
 
 
 
@@ -28,18 +41,52 @@ public class EnemyRespawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // 스폰 시간 0으로 초기화
         timeAfterSpawn = 0f;
+        // 스폰 시간 범위는 Min에서 Max이다
         spawnRate = Random.Range(spawnRateMin, spawnRateMax);
 
-        //enemyArrays = new GameObject[arraysize];
-        spawnposition1 = new Vector3((float)-8.5, (float)0.5, (float)13.5);
-        spawnposition2 = new Vector3((float)-5.5, (float)0.5, (float)13.5);
-        spawnposition3 = new Vector3((float)-2.5, (float)0.5, (float)13.5);
-        //for(int i = 0; i < (enemyArrays.Length) / 2; i++) 
-        //{
-        //    spawnposition1 = new Vector3((float)-8.5 + (i*3), (float)0.5, (float)13.5);
-        //    spawnposition2 = new Vector3((float)0.5 + (i*3), (float)0.5, (float)13.5);
-        //}
+
+        // 스폰 기준점 설정
+        startSpawnLocation1 = new Vector3(-8.5f, 0.5f, 13.5f);
+        startSpawnLocation2 = new Vector3(0.5f, 0.5f, 13.5f);
+
+
+
+        // 중첩 for문 - 좌측 스폰
+        for (int j = 0; j < row; j++)
+        {
+            for (int i = 0; i < column; i++)
+            {
+                startSpawnLocation1.x += spaceX;
+                GameObject enemy = Instantiate(enemyPrefab, startSpawnLocation1, transform.rotation, enemyRespawnPoint1.transform);
+
+
+            }
+            startSpawnLocation1.x = -8.5f;
+            startSpawnLocation1.z -= spaceZ;
+
+        }
+        startSpawnLocation1 = new Vector3(-8.5f, 0.5f, 13.5f);
+
+
+
+        // 홍씨가 한것
+        // 중첩 for문 - 우측 스폰
+        for (int j = 0; j < row; j++)
+        {
+            for (int i = 0; i < column; i++)
+            {
+                startSpawnLocation2.x += spaceX;
+                GameObject enemy = Instantiate(enemyPrefab, startSpawnLocation2, transform.rotation, enemyRespawnPoint2.transform);
+
+
+            }
+            startSpawnLocation2.x = 0.5f;
+            startSpawnLocation2.z -= spaceZ;
+
+        }
+        startSpawnLocation2 = new Vector3(0.5f, 0.5f, 13.5f);
     }
 
     // Update is called once per frame
@@ -50,21 +97,46 @@ public class EnemyRespawner : MonoBehaviour
         if (timeAfterSpawn >= spawnRate)
         {
             timeAfterSpawn = 0f;
-            //GameObject enemy = Instantiate(enemyPrefab, transform.position, transform.rotation);
-            GameObject enemy1 = Instantiate(enemyPrefab, spawnposition1, transform.rotation);
-            GameObject enemy2 = Instantiate(enemyPrefab, spawnposition2, transform.rotation);
-            GameObject enemy3 = Instantiate(enemyPrefab, spawnposition3, transform.rotation);
 
 
-            //for (int i = 0; i < arraysize; i++)
-            //{
-            //    enemyArrays[i] = Instantiate(enemyPrefab, spawnposition1, transform.rotation);
-            //    enemyArrays[i] = Instantiate(enemyPrefab, spawnposition2, transform.rotation);
-            //    enemyArrays[i] = Instantiate(enemyPrefab, spawnposition3, transform.rotation);
-            //}
+
+            // 중첩 for문 - 좌측 스폰
+            for (int j = 0; j < row; j++)
+            {
+                for (int i = 0; i < column; i++)
+                {
+                    startSpawnLocation1.x += spaceX;
+                    GameObject enemy = Instantiate(enemyPrefab, startSpawnLocation1, transform.rotation, enemyRespawnPoint1.transform);
 
 
-            //enemy.transform.LookAt(target);
+                }
+                startSpawnLocation1.x = -8.5f;
+                startSpawnLocation1.z -= spaceZ;
+
+            }
+            startSpawnLocation1 = new Vector3(-8.5f, 0.5f, 13.5f);
+
+
+
+            // 홍씨가 한것
+            // 중첩 for문 - 우측 스폰
+            for (int j = 0; j < row; j++)
+            {
+                for (int i = 0; i < column; i++)
+                {
+                    startSpawnLocation2.x += spaceX;
+                    GameObject enemy = Instantiate(enemyPrefab, startSpawnLocation2, transform.rotation, enemyRespawnPoint2.transform);
+
+
+                }
+                startSpawnLocation2.x = 0.5f;
+                startSpawnLocation2.z -= spaceZ;
+
+            }
+            startSpawnLocation2 = new Vector3(0.5f, 0.5f, 13.5f);
+
+
+
             spawnRate = Random.Range(spawnRateMin, spawnRateMax);
         }
     }
